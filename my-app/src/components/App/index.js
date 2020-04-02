10fe39ee11ac1d545ac1d3ca21ccdaa79d33e72d
Pradeep Kumar <sirkp@gmail.com>
2020-04-03 02:03:25 +0530

    homepage done

---
+++ my-app/src/components/App/index.js
@@ -0,0 +1,30 @@
+// import React, { useState, useEffect } from 'react'
+import React from 'react'
+import './style.css'
+import HomePage from '../HomePage'
+import Login from '../Login'
+import Register from '../Register'
+import Dashboard from '../Dashboard'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import firebase from '../firebase'
+
+const theme = createMuiTheme()
+
+export default function App() {
+
+	return  (
+		<MuiThemeProvider theme={theme}>
+			<CssBaseline />
+			<Router>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/dashboard" component={Dashboard} />
+				</Switch>
+			</Router>
+		</MuiThemeProvider>
+    )
+}
